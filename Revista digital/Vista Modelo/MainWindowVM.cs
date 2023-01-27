22211d521446d9f1c3d7e8d006101a5615e39a14
diazAaron <dbaaron@live.com>
2023-01-27 12:53:23 +0100

    cambios

---
+++ Revista digital/Vista Modelo/MainWindowVM.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Toolkit.Mvvm.ComponentModel;
+using Microsoft.Toolkit.Mvvm.Input;
+using Revista_digital.Modelo;
+using System;
+using System.Collections.ObjectModel;
+using System.Windows.Controls;
+
+namespace Revista_digital.Vista_Modelo
+{
+    class MainWindowVM : ObservableObject
+    {
+        public RelayCommand AbrirCreacionArticulo { get; }
+        
+    }
+}
