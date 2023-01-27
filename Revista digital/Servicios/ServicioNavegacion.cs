e5551e666b4a67ca00e8d5cd0d6a79656b79a57b
diazAaron <dbaaron@live.com>
2023-01-27 14:07:16 +0100

    Enlaces desde MainWindow

--- Revista digital/Servicios/ServicioNavegacion.cs
+++ Revista digital/Servicios/ServicioNavegacion.cs
@@ -4,10 +4,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Revista_digital.Vistas;
+
 
 namespace Revista_digital.Servicios
 {
     class ServicioNavegacion
     {
+        public CreacionArticuloUserControl AbrirCreacionArticulo()
+        {
+            return new CreacionArticuloUserControl();
+        }
+        public bool? AbrirVistaAutor()
+        {
+            VistaAutor vistaAutor = new VistaAutor();
+            return vistaAutor.ShowDialog();
+        }
     }
 }
